f06f72eab462c98b4d3fbec7ed0e71f47839c83c
Leandro Pesqueira <leandropesqueira7@gmail.com>
2023-01-01 17:24:32 -0300

    Deletando uma categoria com DELETE

--- src/main/java/com/udemy/cursomc/services/CategoriaService.java
+++ src/main/java/com/udemy/cursomc/services/CategoriaService.java
@@ -3,6 +3,7 @@ package com.udemy.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.udemy.cursomc.domain.Categoria;
@@ -28,6 +29,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {//Quando o id não está valendo nulo ele atualiza(save)
 		find(obj.getId());
 		return repository.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new com.udemy.cursomc.services.exceptions.DataIntegrityViolationException("Não é possível excluir uma categoria que possui produtos!");
+		}		
 	}
 
 }
